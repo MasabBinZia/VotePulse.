609d25b7af44a14a854aa9f69549e700ac9d2b25
MasabBinZia <masabmbz5@gmail.com>
2024-05-12 01:42:15 +0500

    add table in candidate page

--- frontend/src/pages/Candidates.tsx
+++ frontend/src/pages/Candidates.tsx
@@ -2,6 +2,17 @@ import { useAuth } from "@/Providers/AuthProvider";
 import { BASE_URL } from "../../envConstants";
 import { useQuery } from "@tanstack/react-query";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 const URL = `${BASE_URL}/candidate`;
 
 type Candidate = {
@@ -25,8 +36,11 @@ export default function CandidatesPage() {
   if (error) return <p>Error loading candidates.</p>;
 
   return (
-    <main>
-      {data && data.length > 0 ? (
+    <main className="flex flex-col items-center py-20">
+      <h1 className="text-4xl font-bold text-center">
+        List Of Candidates Available to Vote
+      </h1>
+      {/* {data && data.length > 0 ? (
         <div>
           {data.map((candidate: Candidate, index: number) => (
             <div key={index}>
@@ -36,7 +50,28 @@ export default function CandidatesPage() {
         </div>
       ) : (
         <p>No candidates found.</p>
-      )}
+      )} */}
+
+      <section className="w-[400px] py-12 flex justify-center items-center">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="font-medium text-center">No.</TableHead>
+              <TableHead className="font-medium text-center">Candidate Name</TableHead>
+              <TableHead className="font-medium text-center">Candidate Party</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((candidate: Candidate, idx: number) => (
+              <TableRow key={idx}>
+                <TableCell className="text-center">{idx + 1}</TableCell>
+                <TableCell className="text-center">{candidate.name}</TableCell>
+                <TableCell className="text-center">{candidate.party}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </section>
     </main>
   );
 }
